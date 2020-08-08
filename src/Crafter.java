b969c51852e01a4af5e89a9b0d35e344dad768af
oilboi <47129783+oilboi@users.noreply.github.com>
2020-08-08 00:59:47 -0700

    Clean up Crafter imports

--- src/Crafter.java
+++ src/Crafter.java
@@ -1,14 +1,9 @@
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector2f;
-import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Mesh;
-import com.jme3.scene.VertexBuffer;
-import com.jme3.scene.shape.Box;
 import com.jme3.system.AppSettings;
-import com.jme3.util.BufferUtils;
 
 import java.awt.*;
 
