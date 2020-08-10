7f981883b722eb235d764b9a420b128a06509131
oilboi <47129783+oilboi@users.noreply.github.com>
2020-08-09 23:19:53 -0700

    Optimize chunks a bit more

--- src/ChunkMesh.java
+++ src/ChunkMesh.java
@@ -28,67 +28,43 @@ public class ChunkMesh extends Mesh{
 
             int block = chunk.getBlock(x,y,z);
             if( block > 0) {
+
+                Material matVC = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+                matVC.setBoolean("VertexColor", true);
+
+                Quad quad = new Quad(1, 1); //The quad worker object
+
                 //front
-                Quad quad = new Quad(1, 1); // replace the definition of Vertex and Textures Coordinates plus indexes
                 Geometry geo = new Geometry("OurQuad", quad); // using Quad object
-                //Material mat = new Material(assetManager,
-                //        "Common/MatDefs/Misc/Unshaded.j3md");
-                //mat.setColor("Color", ColorRGBA.White);
-                //geo.setMaterial(mat);
                 geo.setLocalTranslation(0 + x, 0 + y, 0 + z);
                 meshCollection.add(geo);
 
                 //right
-                Quad quad2 = new Quad(1, 1); // replace the definition of Vertex and Textures Coordinates plus indexes
-                Geometry geo2 = new Geometry("OurQuad", quad2); // using Quad object
-//            Material mat2 = new Material(assetManager,
-//                    "Common/MatDefs/Misc/Unshaded.j3md");
-//            mat2.setColor("Color", ColorRGBA.Gray);
-//            geo2.setMaterial(mat2);
+                Geometry geo2 = new Geometry("OurQuad", quad); // using Quad object
                 geo2.setLocalTranslation(1 + x, 0 + y, 0 + z);
                 geo2.rotate(0, (float) Math.PI / 2, 0);
                 meshCollection.add(geo2);
 
                 //back
-                Quad quad3 = new Quad(1, 1); // replace the definition of Vertex and Textures Coordinates plus indexes
-                Geometry geo3 = new Geometry("OurQuad", quad3); // using Quad object
-//            Material mat3 = new Material(assetManager,
-//                    "Common/MatDefs/Misc/Unshaded.j3md");
-//            mat3.setColor("Color", ColorRGBA.Green);
-//            geo3.setMaterial(mat3);
+                Geometry geo3 = new Geometry("OurQuad", quad); // using Quad object
                 geo3.setLocalTranslation(1 + x, 0 + y, -1 + z);
                 geo3.rotate(0, (float) Math.PI, 0);
                 meshCollection.add(geo3);
 
                 //left
-                Quad quad4 = new Quad(1, 1); // replace the definition of Vertex and Textures Coordinates plus indexes
-                Geometry geo4 = new Geometry("OurQuad", quad4); // using Quad object
-//            Material mat4 = new Material(assetManager,
-//                    "Common/MatDefs/Misc/Unshaded.j3md");
-//            mat4.setColor("Color", ColorRGBA.Pink);
-//            geo4.setMaterial(mat4);
+                Geometry geo4 = new Geometry("OurQuad", quad); // using Quad object
                 geo4.setLocalTranslation(0 + x, 0 + y, -1 + z);
                 geo4.rotate(0, (float) Math.PI * (float) 1.5, 0);
                 meshCollection.add(geo4);
 
                 //top
-                Quad quad5 = new Quad(1, 1); // replace the definition of Vertex and Textures Coordinates plus indexes
-                Geometry geo5 = new Geometry("OurQuad", quad5); // using Quad object
-//            Material mat5 = new Material(assetManager,
-//                    "Common/MatDefs/Misc/Unshaded.j3md");
-//            mat5.setColor("Color", ColorRGBA.Yellow);
-//            geo5.setMaterial(mat5);
+                Geometry geo5 = new Geometry("OurQuad", quad); // using Quad object
                 geo5.setLocalTranslation(0 + x, 1 + y, 0 + z);
                 geo5.rotate((float) Math.PI * (float) 1.5, 0, 0);
                 meshCollection.add(geo5);
 
                 //bottom
-                Quad quad6 = new Quad(1, 1); // replace the definition of Vertex and Textures Coordinates plus indexes
-                Geometry geo6 = new Geometry("OurQuad", quad6); // using Quad object
-//            Material mat6 = new Material(assetManager,
-//                    "Common/MatDefs/Misc/Unshaded.j3md");
-//            mat6.setColor("Color", ColorRGBA.Red);
-//            geo6.setMaterial(mat6);
+                Geometry geo6 = new Geometry("OurQuad", quad); // using Quad object
                 geo6.setLocalTranslation(0 + x, 0 + y, 0 + z);
                 geo6.rotate((float) Math.PI / 2, (float) Math.PI / 2, 0);
                 meshCollection.add(geo6);
@@ -107,6 +83,7 @@ public class ChunkMesh extends Mesh{
         Mesh chunky = new Mesh();
 
         GeometryBatchFactory.mergeGeometries(meshCollection,chunky);
+
 
         Geometry geo = new Geometry("test", chunky);
 
--- src/Crafter.java
+++ src/Crafter.java
@@ -1,12 +1,15 @@
 import com.jme3.app.SimpleApplication;
 import com.jme3.scene.Geometry;
 import com.jme3.system.AppSettings;
+import org.lwjgl.opengl.Display;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Random;
 
 public class Crafter extends SimpleApplication {
     //the working directory of the game
@@ -49,11 +52,29 @@ public class Crafter extends SimpleApplication {
 
         flyCam.setMoveSpeed(100);
 
-        Chunk chunky = new Chunk();
-
-        Geometry geo = ChunkMesh.genChunkMesh(chunky, assetManager);
-
-        rootNode.attachChild(geo);
-
     }
+
+    private int count = 0;
+    @Override
+    public void simpleUpdate(float tpf){
+
+//        if (count < 20) {
+//            count++;
+//            Chunk chunky = new Chunk();
+//
+//            Geometry geo = ChunkMesh.genChunkMesh(chunky, assetManager);
+//
+//            rootNode.attachChild(geo);
+//        }
+
+//        Display.setLocation((int)(Math.random()*1000), (int)(Math.random() * 1000));
+//
+//        byte[] array = new byte[30];
+//        new Random().nextBytes(array);
+//        String generatedString = new String(array, Charset.forName("UTF-8"));
+//
+//        Display.setTitle(generatedString);
+    }
+
+
 }
